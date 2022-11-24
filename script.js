27f09828bce4c895dc4f2d6c5ce79dea09c03a38
Jorm <charkuayteow.10@gmail.com>
2022-11-24 13:06:57 +0800

    deleted basically everything and started over

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName("togglebutton")[0];
-const navbarLinks = document.getElementsByClassName("itemdisappear")[0];
-
-toggleButton.addEventListener("click", () => {
-  navbarLinks.classList.toggle("active");
-});
